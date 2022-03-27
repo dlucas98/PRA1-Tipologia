644c821e86e1c315ca88665dec10e8c41c59616c
David Lucas <david.luto98@gmail.com>
2022-03-27 14:11:51 +0200

    Getting games url

--- Prueba_entrar_web.py
+++ Prueba_entrar_web.py
@@ -11,4 +11,25 @@ contenido = page.content
 soup = BeautifulSoup(contenido)
 
 with open("content.txt", "w") as f:
-    f.write(str(soup.prettify()))
+    # f.write(str(soup.prettify()))
+    f.write(str(soup.find_all("a")))
+
+lis = soup.find_all("li")
+
+games = []
+
+for li in lis:
+    a = li.next_element
+    if a.name == 'a':
+        href = a['href']
+        if href[0] == '/':
+            if (href.find("box-score#box-score") != -1):
+                games.append(href)
+            
+print(len(games))
+
+
+
+
+# print(lis.find("href"))
+# soup.find(data-id="nba:games:main:box-score:cta")
