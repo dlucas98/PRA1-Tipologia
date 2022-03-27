91f60258a7dd65593e07171637cdf349ade5aae0
dlucas98 <david.luto98@gmail.com>
2022-03-27 12:22:47 +0200

    Prueba para entrar en la web

---
+++ Prueba_entrar_web.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+import pandas as pd
+
+driver_path = 'E:\Documentos\Selenium\chromedriver.exe'
+driver = webdriver.Chrome(driver_path)
+#driver.get('https://www.nba.com/games?date=2022-03-26')
